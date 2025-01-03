d24c6db4021dbd5f96293ddc8f31b4bcb120074f
nika <nikalobjanidze014@gmail.com>
2025-01-03 23:06:39 +0400

    chamoshils funqciebi

--- script/app.js
+++ script/app.js
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
             "modalRegistrationQuestion": "არ გაქვს ანგარიში?",
             "modalRegistrationText": "რეგისტრაცია",
             "modalAuthButton": "ავტორიზაცია",
-            /// aklia
             "closeErrorButton": "დახურვა"
         },
         en: {
@@ -36,7 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
             "modalRegistrationQuestion": "Don't have an account?",
             "modalRegistrationText": "Sign Up",
             "modalAuthButton": "Authorize",
-            /// aklia
             "closeErrorButton": "Close"
         },
         ru: {
@@ -53,7 +51,6 @@ document.addEventListener("DOMContentLoaded", () => {
             "modalRegistrationQuestion": "Нет аккаунта?",
             "modalRegistrationText": "Регистрация",
             "modalAuthButton": "Авторизоваться",
-            /// aklia
             "closeErrorButton": "Закрыть"
         }
     };
@@ -303,3 +300,248 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /// MODAL AUTHORIZATION BUTTON /// MODAL AUTHORIZATION BUTTON /// MODAL AUTHORIZATION BUTTON 
 
+
+
+
+/// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY
+/// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY
+document.addEventListener('click', function(event) {
+  let dropdownBox1 = document.querySelector('.ticket-dropdowns-container-1-box');
+  let dropdownContent1 = document.getElementById('dropdown1');
+  let isDropdownClick1 = dropdownBox1.contains(event.target);
+  let dropdownBox2 = document.querySelector('.ticket-dropdowns-container-2-box');
+  let dropdownContent2 = document.getElementById('dropdown2');
+  let isDropdownClick2 = dropdownBox2.contains(event.target);
+  let dropdownBox3 = document.querySelector('.ticket-dropdowns-container-3-box');
+  let dropdownContent3 = document.getElementById('dropdown3');
+  let isDropdownClick3 = dropdownBox3.contains(event.target);
+  if (!isDropdownClick1 && dropdownContent1.classList.contains('dropdown-visible')) {
+    closeDropdown(dropdownContent1, dropdownBox1);
+  }
+  if (!isDropdownClick2 && dropdownContent2.classList.contains('dropdown-visible')) {
+    closeDropdown(dropdownContent2, dropdownBox2);
+  }
+  if (!isDropdownClick3 && dropdownContent3.classList.contains('dropdown-visible')) {
+    closeDropdown(dropdownContent3, dropdownBox3);
+  }
+  if ((event.target.id === 'delete-button-1' || event.target.id === 'confirm-button-1') && dropdownContent2.classList.contains('dropdown-visible')) {
+    alert(`${event.target.id} button clicked!`);
+    closeDropdown(dropdownContent2, dropdownBox2);
+  }
+});
+function toggleDropdown(dropdownId, boxElement) {
+  let dropdown = document.getElementById(dropdownId);
+  let caretIcon = boxElement.querySelector('.ticket-dropdowns-container-1-box-caret-icon i');
+  let isVisible = dropdown.classList.contains('dropdown-visible');
+  closeAllDropdowns();
+  if (!isVisible) {
+    dropdown.classList.add('dropdown-visible');
+    caretIcon.classList.replace('bi-caret-down-fill', 'bi-caret-up-fill');
+    boxElement.classList.add('dropdown-open');
+  } else {
+    dropdown.classList.remove('dropdown-visible');
+    caretIcon.classList.replace('bi-caret-up-fill', 'bi-caret-down-fill');
+    boxElement.classList.remove('dropdown-open');
+  }
+}
+function closeAllDropdowns() {
+  let allDropdowns = document.querySelectorAll('.dropdown');
+  let allCarets = document.querySelectorAll('.ticket-dropdowns-container-1-box-caret-icon i');
+  let allBoxes = document.querySelectorAll('.ticket-dropdowns-container-1-box, .ticket-dropdowns-container-2-box, .ticket-dropdowns-container-3-box');
+  allDropdowns.forEach(d => d.classList.remove('dropdown-visible'));
+  allCarets.forEach(caret => caret.classList.replace('bi-caret-up-fill', 'bi-caret-down-fill'));
+  allBoxes.forEach(box => box.classList.remove('dropdown-open'));
+}
+function closeDropdown(dropdown, dropdownBox) {
+  dropdown.classList.remove('dropdown-visible');
+  let caretIcon = dropdownBox.querySelector('.ticket-dropdowns-container-1-box-caret-icon i');
+  caretIcon.classList.replace('bi-caret-up-fill', 'bi-caret-down-fill');
+  dropdownBox.classList.remove('dropdown-open');
+}
+function selectOption(optionElement) {
+  let box = optionElement.closest('.ticket-dropdowns-container-1-box');
+  let boxText = box.querySelector('.ticket-dropdowns-container-1-box-text');
+  let boxIcon = box.querySelector('.ticket-dropdowns-container-1-box-repeat-icon i');
+  let selectedText = optionElement.querySelector('.dropdown-content-text').textContent;
+  let selectedIconClass = optionElement.querySelector('.dropdown-content-icon-1 i').classList[1];
+  boxText.textContent = selectedText;
+  boxIcon.className = `bi ${selectedIconClass}`;
+  closeAllDropdowns();
+}
+let dropdownContent2 = document.getElementById('dropdown2');
+dropdownContent2.addEventListener('click', function(event) {
+  if (event.target.id !== 'delete-button-1' && event.target.id !== 'confirm-button-1') {
+    event.stopPropagation();
+  }
+});
+
+
+/// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY
+/// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY /// DROPDOWNS FUNCTIONALITY
+
+/// FIRST SECTION OF NUMBERS CUBE /// FIRST SECTION OF NUMBERS CUBE /// FIRST SECTION OF NUMBERS CUBE /// FIRST SECTION OF NUMBERS CUBE
+let counterElement = document.getElementById('counter');
+let reduceCube = document.getElementById('reduce-cube');
+let increaseCube = document.getElementById('increase-cube');
+let counterValue = parseInt(counterElement.innerText);
+reduceCube.addEventListener('click', function() {
+    if (counterValue > 1) { 
+        counterValue--;
+        counterElement.innerText = counterValue;
+        increaseCube.style.backgroundColor = '';  
+    }
+});
+increaseCube.addEventListener('click', function() {
+    if (counterValue < 8) {  // Limit the value to 8
+        counterValue++;
+        counterElement.innerText = counterValue;
+    }
+    if (counterValue === 8) {
+        increaseCube.style.backgroundColor = '#a5a5a5'; 
+    } else {
+        increaseCube.style.backgroundColor = ''; 
+    }
+});
+let numberOptionCube1 = document.getElementById('reduce-cube');
+numberOptionCube1.addEventListener('click', function() {
+    numberOptionCube1.classList.add('clicked'); 
+    setTimeout(function() {
+        numberOptionCube1.classList.remove('clicked');
+    }, 200); 
+});
+let numberOptionCube2 = document.getElementById('increase-cube');
+numberOptionCube2.addEventListener('click', function() {
+    numberOptionCube2.classList.add('clicked'); 
+    setTimeout(function() {
+        numberOptionCube2.classList.remove('clicked');
+    }, 200); 
+});
+/// FIRST SECTION OF NUMBERS CUBE /// FIRST SECTION OF NUMBERS CUBE /// FIRST SECTION OF NUMBERS CUBE /// FIRST SECTION OF NUMBERS CUBE
+/// SECOND SECTION OF NUMBERS CUBE /// SECOND SECTION OF NUMBERS CUBE /// SECOND SECTION OF NUMBERS CUBE /// SECOND SECTION OF NUMBERS CUBE
+let counterElement2 = document.getElementById('counter-2');
+let reduceCube2 = document.getElementById('reduce-cube-2');
+let increaseCube2 = document.getElementById('increase-cube-2');
+let counterValue2 = parseInt(counterElement2.innerText);
+
+reduceCube2.addEventListener('click', function() {
+    if (counterValue2 > 0) { 
+        counterValue2--; 
+        counterElement2.innerText = counterValue2;
+        increaseCube2.style.backgroundColor = ''; 
+    }
+});
+increaseCube2.addEventListener('click', function() {
+    if (counterValue2 < 8) {  
+        counterValue2++;  
+        counterElement2.innerText = counterValue2;
+    }
+    if (counterValue2 === 8) {
+        increaseCube2.style.backgroundColor = '#a5a5a5';  
+    } else {
+        increaseCube2.style.backgroundColor = ''; 
+    }
+});
+let numberOptionCube = document.getElementById('reduce-cube-2');
+numberOptionCube.addEventListener('click', function() {
+    numberOptionCube.classList.add('clicked');
+    setTimeout(function() {
+        numberOptionCube.classList.remove('clicked');
+    }, 200);  
+});
+let numberOptionCube3 = document.getElementById('increase-cube-2');
+numberOptionCube3.addEventListener('click', function() {
+    numberOptionCube3.classList.add('clicked'); 
+    setTimeout(function() {
+        numberOptionCube3.classList.remove('clicked');
+    }, 200); 
+});
+/// SECOND SECTION OF NUMBERS CUBE /// SECOND SECTION OF NUMBERS CUBE /// SECOND SECTION OF NUMBERS CUBE /// SECOND SECTION OF NUMBERS CUBE
+/// THIRD SECTION OF NUMBERS CUBE /// THIRD SECTION OF NUMBERS CUBE /// THIRD SECTION OF NUMBERS CUBE /// THIRD SECTION OF NUMBERS CUBE
+let counterElement4 = document.getElementById('counter-4');
+let reduceCube4 = document.getElementById('reduce-cube-4');
+let increaseCube4 = document.getElementById('increase-cube-4');
+let counterValue4 = parseInt(counterElement4.innerText);
+
+reduceCube4.addEventListener('click', function() {
+    if (counterValue4 > 0) { 
+        counterValue4--;
+        counterElement4.innerText = counterValue4;
+        increaseCube4.style.backgroundColor = '';  
+    }
+    reduceCube4.classList.add('clicked');
+    setTimeout(function() {
+        reduceCube4.classList.remove('clicked');
+    }, 200); 
+});
+increaseCube4.addEventListener('click', function() {
+    if (counterValue4 < 2) { 
+        counterValue4++;
+        counterElement4.innerText = counterValue4;
+    }
+    if (counterValue4 === 2) {
+        increaseCube4.style.backgroundColor = '#a5a5a5';
+    } else {
+        increaseCube4.style.backgroundColor = '';  
+    }
+    increaseCube4.classList.add('clicked');
+    setTimeout(function() {
+        increaseCube4.classList.remove('clicked');
+    }, 200); 
+});
+/// THIRD SECTION OF NUMBERS CUBE /// THIRD SECTION OF NUMBERS CUBE /// THIRD SECTION OF NUMBERS CUBE /// THIRD SECTION OF NUMBERS CUBE
+
+
+document.querySelectorAll('.checkbox').forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+      if (this.checked) {
+          // Uncheck other checkboxes
+          document.querySelectorAll('.checkbox').forEach(otherCheckbox => {
+              if (otherCheckbox !== this) {
+                  otherCheckbox.checked = false;
+              }
+          });
+      }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+// fetch('TicketsResults.json') 
+// .then(response => {
+//     if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+// })
+// .then(data => {
+//     let ticketList = document.getElementById('ticket-list');
+//     data.forEach(ticket => {
+//         console.log('Ticket:', ticket);
+//         let ticketDiv = document.createElement('div');
+//         ticketDiv.classList.add('ticket');
+//         ticketDiv.innerHTML = `
+//             <h3>Route No: ${ticket.RouteNo}</h3>
+//             <p><strong>Airline:</strong> ${ticket.Airline}</p>
+//             <p><strong>From:</strong> ${ticket.From}</p>
+//             <p><strong>To:</strong> ${ticket.To}</p>
+//             <p><strong>Transfer City:</strong> ${ticket['Transfer City']}</p>
+//             <p><strong>Departure:</strong> ${ticket.Departure}</p>
+//             <p><strong>Arrival:</strong> ${ticket.Arrival}</p>
+//             <p><strong>Transfer Route No:</strong> ${ticket.TransferRouteNo}</p>
+//             <p><strong>Transfer Departure:</strong> ${ticket['Transfer Departure']}</p>
+//             <p><strong>Transfer Arrival:</strong> ${ticket['Transfer Arrival']}</p>
+//             <p><strong>Transfer Duration:</strong> ${ticket['Transfer Duration']}</p>
+//             <p><strong>Total Duration:</strong> ${ticket['Total Duration']}</p>
+//             <p><strong>Economy Price:</strong> $${ticket['Economy Price']}</p>
+//             <p><strong>Business Price:</strong> $${ticket['Business Price']}</p>
+//         `;
+//         ticketList.appendChild(ticketDiv);
+//     });
+// });
